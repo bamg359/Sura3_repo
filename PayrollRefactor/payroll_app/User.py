3aeadf4e67eedda2d0685e0b1f45c74dbb34d465
JavierBoyaca <68155136+bamg359@users.noreply.github.com>
2024-06-29 14:30:08 -0500

    Data
    Data

---
+++ PayrollRefactor/payroll_app/User.py
@@ -0,0 +1,49 @@
+
+
+class User:
+
+
+    employee_id = None
+    name_employee = None
+    last_name_employee = None
+    email = None
+    password = None
+
+
+    # comment
+
+    def __init__(self, employee_id, name_employee, last_name_employee, email, password):
+        self._employee_id = employee_id
+        self._name_employee = name_employee
+        self._last_name_employee = last_name_employee
+        self._email = email
+        self._password = password
+
+
+    @property
+    def employee_id(self):
+        return self._employee_id
+
+    @employee_id.setter
+    def employee_id(self, employee_id):
+        self._employee_id = employee_id
+
+    employees = []
+
+    def create_user(self, employees):
+        #employee = []
+        self._employee_id = input("Id Empleado")
+        #employee.append(self._employee_id)
+        self._name_employee = input("Nombre empleado")
+        #employee.append(self._name_employee)
+        self._last_name_employee = input("Apellido empleado")
+        #employee.append(self._last_name_employee)
+        self._email = input("Email")
+        #employee.append(self._email)
+        self._password = input("Contraseña")
+        #employee.append(self._password)
+        #employees.append(employee)
+
+    def list_employee_data(self, employees):
+        for item in employees:
+            print(item)
